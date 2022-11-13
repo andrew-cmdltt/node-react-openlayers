5ef225a03534e2ffecd0f8579d4fde9d50159f74
andrew-cmdltt <menwhohas2279@gmail.com>
2022-11-13 19:48:35 +0300

    make it better

--- src/utils/getLocations.js
+++ src/utils/getLocations.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { tour } from "./tour";
 
 export function getLocations(view) {
-    axios.get("http://localhost:8080/api/locations")
+    axios.get("https://node-react-openlayers.herokuapp.com/api/locations")
         .then(res => {
             tour(res.data, view)
         })
